65ce3d979151a377c6ba04d5586e173a15c73006
windows <windows@microsoft.com>
2022-12-06 20:57:44 +0530

    change

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        <h1>welcome react js</h1>
+       <h2>this is heading two</h2>
       </header>
     </div>
   );
