51f476ead09f259be44bed1b17e6a3a0c392b6da
windows <windows@microsoft.com>
2022-12-19 21:05:28 +0530

    add bootstra and routing

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>welcome to home components</div>
+  )
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Welcome from './components/Welcome';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Welcome></Welcome>
+    <BrowserRouter>
+      <Routes>
+          <Route path='' element={<Welcome />}/>
+          <Route path='home' element={<Home/>}/>
+          <Route path='about' element={<About />} />
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
