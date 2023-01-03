c93de887f6ec67fe8ec675fa34115ae2a5094973
windows <windows@microsoft.com>
2023-01-03 20:54:50 +0530

    axiox install

--- src/components/DataFatch.js
+++ src/components/DataFatch.js
@@ -1,26 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function DataFatch() {
   
 //     axios.get('https://jsonplaceholder.typicode.com/posts').then( (res)=>{
 //     const mydata = res.data;  
 //   console.log(res.data);
+const [datavariable,datafunction]=useState([]);
 
 useEffect(()=>{
-    axios.get('https://jsonplaceholder.typicode.com/posts').then( (res)=>{
-        const mydata = res.data;  
-    //   console.log(res.data);
-      console.log(mydata);
-});
+    axios.get('https://jsonplaceholder.typicode.com/photos').then((res)=>{
+        // console.log(res.data);
+        datafunction(res.data);
+    })
 
   });
   
     return (
-    <div>
-     
+      <div className='container-fluid'>
+      <div className='row'>
+      {datavariable.map((r)=>(
+            <div className='col-3 mt-2' key={r.id}>
+            <div className="card">
+              <div className="card-body">
+                <img src={r.thumbnailUrl} alt="imgs" />
+                <h5 className="card-title">{r.title}</h5>
+                <p className="card-text">{r.body}</p>
+                <h1 className='text-center'>{r.albumId}</h1>
+                <a href="#" className="btn btn-primary">{r.id} Go somewhere</a>
+              </div>
+            </div>
+            </div>
+      ))}
 
-
+    </div>
     </div>
   )
 }
