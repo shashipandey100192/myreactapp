edf92bee5b346d51b673b72f44368f4e2a534c0c
shashi <shashiweb64@gmail.com>
2026-08-18 20:03:33 +0530

    change

--- src/modules/users/auth/Userlogin.js
+++ src/modules/users/auth/Userlogin.js
@@ -1,24 +1,28 @@
 import React,{useState} from 'react'
+import { useForm } from "react-hook-form"
 
 function Userlogin() {
+    const {register,handleSubmit,formState: { errors }} = useForm()
+
+const myform = (myformdata)=>{
+        console.log(myformdata);
+}
+
 
     const [show, setShow] = useState(false);
 
     return (
 
+        <form onSubmit={handleSubmit(myform)}>
         <div className="container-fluid login-bg">
-
             <div className="row vh-100 justify-content-center align-items-center">
-
                 <div className="col-12">
-
                     <div className="glass-card p-5">
-
                         <h2 className="text-center text-white mb-4">
                             User Login
                         </h2>
 
-                        <form>
+                      
 
                             <div className="mb-4">
                                 <label className="form-label text-white">
@@ -29,7 +33,9 @@ function Userlogin() {
                                     type="text"
                                     className="form-control form-control-lg"
                                     placeholder="Enter Username"
-                                />
+                                    {...register("username",{required:true})} />
+                                    {errors.username && <p className="text-warning">User name is required</p>}
+                            
                             </div>
 
                             <div className="mb-4">
@@ -44,29 +50,33 @@ function Userlogin() {
                                         type={show ? "text" : "password"}
                                         className="form-control form-control-lg"
                                         placeholder="Enter Password"
-                                    />
+                                        {...register("pass",{required:true,minLength:5,maxLength:10,pattern:/^[A-Z0-9a-z]+$/g})}/>
+                                        
 
                                     <button
                                         type="button"
                                         className="btn btn-light"
-                                        onClick={() => setShow(!show)}
-                                    >
+                                        onClick={() => setShow(!show)}>
                                         <i className={`bi ${show ? "bi-eye-slash" : "bi-eye"}`}></i>
                                     </button>
-
                                 </div>
+                                {errors.pass?.type==="required" && <p className='text-warning'> password is required</p>}
+                                 {errors.pass?.type==="maxLength" && <p className='text-warning'> too logn</p>}
+                                  {errors.pass?.type==="minLength" && <p className='text-warning'>weak password</p>}
+                                   {errors.pass?.type==="pattern" && <p className='text-warning'> mismatch pattern </p>}
+
 
                             </div>
 
                             <div className="d-grid">
 
-                                <button className="btn login-btn btn-lg">
+                                <button className="btn login-btn btn-lg" type='submit'>
                                     Login
                                 </button>
 
                             </div>
 
-                        </form>
+                 
 
                     </div>
 
@@ -75,6 +85,7 @@ function Userlogin() {
             </div>
 
         </div>
+        </form>
 
     );
 }
